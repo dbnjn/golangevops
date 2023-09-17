ec19b7ac65a0e99760b25a4259d9abb39b5eb6e6
dbnjn <dbnjngoswami@gmail.com>
2023-09-17 16:41:22 +0530

    initial commit

---
+++ my-project.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+// Hello returns a greeting for the named person.
+func Hello(name string) string {
+	// Return a greeting that embeds the name in a message.
+	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	return message
+}
+func main() {
+	// Get a greeting message and print it.
+	message := Hello("Debanjan")
+	fmt.Println(message)
+}
